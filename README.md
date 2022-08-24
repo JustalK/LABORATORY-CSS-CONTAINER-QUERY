b5270133125fdc5e2de8e1140aef885f76d72ab8
justalk <justal.kevin@gmail.com>
2022-08-24 21:44:35 +0200

    [FEATURE] Adding first css test

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # LABORATORY-CSS-CONTAINER-QUERY
+
+```
+CSS container queries is an experimental feature. To enable its support in DevTools, under chrome://flags, set the Enable CSS Container Queries experiment to Enabled.
+```
---
+++ project/apps/app/src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import './style.css';
+
+export function App() {
+  return (
+    <>
+      <div className="parent">
+        <div className="child"></div>
+      </div>
+    </>
+  );
+}
+
+export default App;
