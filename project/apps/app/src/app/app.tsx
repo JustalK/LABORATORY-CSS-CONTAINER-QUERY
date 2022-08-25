264e9839a2bc0ab4692fb71b05769927ebc006d8
justalk <justal.kevin@gmail.com>
2022-08-25 08:21:34 +0200

    [FEATURE] Adding new components

--- project/apps/app/src/app/app.tsx
+++ project/apps/app/src/app/app.tsx
@@ -1,13 +1,30 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
+import Component1 from './components/Component1';
 import './style.css';
 
 export function App() {
   return (
-    <>
-      <div className="parent">
-        <div className="child"></div>
+    <div className="elements">
+      <div className="page">
+        <div className="top" />
+        <div className="bottom">
+          <ul className="tags">
+            {Array.from({ length: 20 }).map(() => {
+              const size = Math.floor(Math.random() * 20 + 2);
+              return <li className="tag" style={{ width: `${size}%` }} />;
+            })}
+          </ul>
+          <Component1 />
+          <span className="words">
+            <span className="word" />
+            {Array.from({ length: 200 }).map(() => {
+              const size = Math.floor(Math.random() * 200 + 2);
+              return <li className="word" style={{ width: `${size}px` }} />;
+            })}
+          </span>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
 
---
+++ project/apps/app/src/app/components/Component1/index.tsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import './style.css';
+
+export function Component1() {
+  return (
+    <div className="component1">
+      <div className="component1_child" />
+    </div>
+  );
+}
+
+export default Component1;
